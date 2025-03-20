07a2d0857f69bc6b5318f6c183474f61afb7710a
louiseh0 <132346318+louiseh0@users.noreply.github.com>
2025-03-20 15:02:21 +0000

    Update dbo.Categories.sql
    New column

--- schema-model/Tables/dbo.Categories.sql
+++ schema-model/Tables/dbo.Categories.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[Categories]
 [CategoryName] [varchar] (15) NULL,
 [Description] [text] NULL,
 [Picture] [varbinary] (max) NULL,
-[Collection] [int] NULL
+[Collection] [int] NULL,
+[Collection2] [int] NULL
 )
 GO
 ALTER TABLE [dbo].[Categories] ADD CONSTRAINT [PK__Categori__19093A2BE949493E] PRIMARY KEY CLUSTERED ([CategoryID])
